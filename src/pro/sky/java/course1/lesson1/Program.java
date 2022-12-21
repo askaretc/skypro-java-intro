bf460597d903cd81709dff42b90e42fb1fc41ed8
askaretc <askaretc@yandex.ru>
2022-12-21 21:00:52 +0300

    Lesson 1 / Homework 2

--- src/pro/sky/java/course1/lesson1/Program.java
+++ src/pro/sky/java/course1/lesson1/Program.java
@@ -32,5 +32,29 @@ public class Program {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+
+
+        ////////// HOMEWORK 2 //////////
+        System.out.println("\n\nHOMEWORK 2");
+
+        ////////// Task 4 //////////
+        System.out.println("\nTask 4:");
+        var friend = 19;
+        System.out.println(friend);
+        friend += 2;
+        System.out.println(friend);
+        friend /= 7;
+        System.out.println(friend);
+
+        ////////// Task 5 //////////
+        System.out.println("\nTask 5:");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog *= 10;
+        System.out.println(frog);
+        frog /= 3.5;
+        System.out.println(frog);
+        frog += 4;
+        System.out.println(frog);
     }
 }
