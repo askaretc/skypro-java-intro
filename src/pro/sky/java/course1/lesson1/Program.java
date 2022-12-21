a76ca3da59b46f4e0dc27d04e727f367e8107aad
askaretc <askaretc@yandex.ru>
2022-12-21 20:55:49 +0300

    Lesson 1 / Homework 1

---
+++ src/pro/sky/java/course1/lesson1/Program.java
@@ -0,0 +1,36 @@
+package pro.sky.java.course1.lesson1;
+
+public class Program {
+    public static void main(String[] args) {
+
+        ////////// HOMEWORK 1 //////////
+        System.out.println("HOMEWORK 1");
+
+        ////////// Task 1 //////////
+        System.out.println("\nTask 1:");
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        ////////// Task 2 //////////
+        System.out.println("\nTask 2:");
+        dog += 4;
+        cat += 4;
+        paper += 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        ////////// Task 3 //////////
+        System.out.println("\nTask 3:");
+        dog -= 3;
+        cat -= 1.6;
+        paper -= 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+}
