71c2934f2302a6c2a77e1cf91b7db0bbc528ca40
askaretc <askaretc@yandex.ru>
2023-01-09 11:51:45 +0300

    Lesson 5 / Homework 2

--- src/pro/sky/java/course1/lesson5/Program.java
+++ src/pro/sky/java/course1/lesson5/Program.java
@@ -32,5 +32,32 @@ public class Program {
 
         for (int i = 10; i >= -10; i--)
             System.out.println(i);
+
+
+        ////////// HOMEWORK 2 //////////
+        System.out.println("\n\nHOMEWORK 2");
+
+        ////////// Task 1 //////////
+        System.out.println("\nTask 1:");
+
+        for (int i = 1904; i <= 2096; i++) {
+            if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
+                System.out.println(i + " год является високосным");
+            }
+        }
+
+
+        ////////// Task 2 //////////
+        System.out.println("\nTask 2:");
+
+        for (int i = 7; i <= 98; i += 7)
+            System.out.println(i);
+
+
+        ////////// Task 3 //////////
+        System.out.println("\nTask 3:");
+
+        for (int i = 1; i <= 512; i *= 2)
+            System.out.println(i);
     }
 }
