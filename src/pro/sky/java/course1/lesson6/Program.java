14c986877104e9c668497eb5969654b517329e6e
askaretc <askaretc@yandex.ru>
2023-01-11 19:30:17 +0300

    Lesson 6 / Homework 2

--- src/pro/sky/java/course1/lesson6/Program.java
+++ src/pro/sky/java/course1/lesson6/Program.java
@@ -46,5 +46,65 @@ public class Program {
             System.out.println("Год " + yearNumber + ", численность населения составляет " + population);
             yearNumber++;
         }
+
+
+        ////////// HOMEWORK 2 //////////
+        System.out.println("\n\nHOMEWORK 2");
+
+        ////////// Task 1 //////////
+        System.out.println("\nTask 1:");
+
+        int depositInterest = 7;
+        int deposit = 15_000;
+        int month = 1;
+        while (deposit < 12_000_000) {
+            deposit += (depositInterest * deposit) / 100;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + deposit + " рублей");
+            month++;
+        }
+
+
+        ////////// Task 2 //////////
+        System.out.println("\n\nTask 2:");
+
+        // Возвращаем исходные значения переменным
+        deposit = 15_000;
+        month = 1;
+        while (deposit < 12_000_000) {
+            // Переменная depositInterest инициализирована в Задаче 1 (Домашнее задание - 2)
+            deposit += (depositInterest * deposit) / 100;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + deposit + " рублей");
+            }
+            month++;
+        }
+
+
+        ////////// Task 3 //////////
+        System.out.println("\n\nTask 3:");
+
+        int yearsAmount = 9;
+        int monthsAmount = yearsAmount * 12;
+        // Возвращаем исходные значения переменным
+        deposit = 15_000;
+        month = 1;
+        while (month <= monthsAmount) {
+            // Переменная depositInterest инициализирована в Задаче 1 (Домашнее задание - 2)
+            deposit += (depositInterest * deposit) / 100;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + deposit + " рублей");
+            }
+            month++;
+        }
+
+
+        ////////// Task 4 //////////
+        System.out.println("\n\nTask 4:");
+
+        int friday = 7;     // номер первой пятницы месяца
+        int daysAmountInAMonth = 31;    // количество дней в месяце
+        for (; friday <= daysAmountInAMonth; friday += 7) {
+            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
+        }
     }
 }
