9e8ded359ff167a576bf55794ac4770683ee4180
askaretc <askaretc@yandex.ru>
2023-02-09 08:10:35 +0300

    Lesson 11 / Homework

---
+++ src/pro/sky/java/course1/lesson11/Author.java
@@ -0,0 +1,22 @@
+package pro.sky.java.course1.lesson11;
+
+public class Author {
+
+    private final String firstName;
+    private final String lastName;
+
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
