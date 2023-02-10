b45a0f01566a3ce356d525e2bc25357f5a64ab4f
askaretc <askaretc@yandex.ru>
2023-02-10 09:10:13 +0300

    Additional tasks 8

--- src/pro/sky/java/course1/lesson11/Program.java
+++ src/pro/sky/java/course1/lesson11/Program.java
@@ -2,29 +2,39 @@ package pro.sky.java.course1.lesson11;
 
 public class Program {
 
-    private static final Book[] booksArray = new Book[10];
+    //private static final Book[] booksArray = new Book[10];
 
     public static void main(String[] args) {
 
-        Author author1 = new Author("Джордж", "Мартин");
-        Author author2 = new Author("Анджей", "Сапковский");
+        Author author1 = new Author("George", "Martin");
+        Author author2 = new Author("Andrzej", "Sapkowski");
 
-        Book book1 = new Book("Игра престолов", author1, 2019);
-        Book book2 = new Book("Ведьмак", author2, 2020);
+        Book book1 = new Book("Game of Thrones", author1, 2019);
+        Book book2 = new Book("The Witcher", author2, 2020);
 
         book2.setPublishingYear(2021);
 
         //System.out.println(book1.getName() + ". " + book1.getAuthorFullName() + ", " + book1.getPublishingYear());
         //System.out.println(book2.getName() + ". " + book2.getAuthorFullName() + ", " + book2.getPublishingYear());
 
-        setBookToArray(book1);
-        setBookToArray(book2);
+        //setBookToArray(book1);
+        //setBookToArray(book2);
 
-        printAllBooks();
+        //printAllBooks();
+
+        Library library = new Library(10);
+
+        library.setBookToLibrary(book1);
+        library.setBookToLibrary(book2);
+
+        library.printInformationAboutBook("The Witcher");
+        library.setPublishingYearByName("The Witcher", 2022);
+
+        library.printAllBooks();
     }
 
 
-    private static void setBookToArray(Book book) {
+    /*private static void setBookToArray(Book book) {
         booksArray[getIndexOfFreeElement()] = book;
     }
 
@@ -60,5 +70,5 @@ public class Program {
             throw new RuntimeException("Нечего выводить! Массив пустой.");
         }
         return false;
-    }
+    }*/
 }
