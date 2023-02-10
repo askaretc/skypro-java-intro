8faf1d178986ea617b5108996b2c291e1d56b12a
askaretc <askaretc@yandex.ru>
2023-02-10 09:25:05 +0300

    Lesson 12 / Homework

--- src/pro/sky/java/course1/lesson11/Author.java
+++ src/pro/sky/java/course1/lesson11/Author.java
@@ -1,5 +1,7 @@
 package pro.sky.java.course1.lesson11;
 
+import java.util.Objects;
+
 public class Author {
 
     private final String firstName;
@@ -18,5 +20,24 @@ public class Author {
 
     public String getLastName() {
         return lastName;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return firstName.equals(author.firstName) && lastName.equals(author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
     }
 }
--- src/pro/sky/java/course1/lesson11/Book.java
+++ src/pro/sky/java/course1/lesson11/Book.java
@@ -1,5 +1,7 @@
 package pro.sky.java.course1.lesson11;
 
+import java.util.Objects;
+
 public class Book {
 
     private final String name;
@@ -27,6 +29,28 @@ public class Book {
     }
 
     public void setPublishingYear(int publishingYear) {
+        if (publishingYear < 0) {
+            throw new IllegalArgumentException("The year of publication cannot be negative.");
+        }
         this.publishingYear = publishingYear;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return name.equals(book.name) && authorFullName.equals(book.authorFullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, authorFullName);
+    }
+
+    @Override
+    public String toString() {
+        return authorFullName.toString() + ": " + name + ": " + publishingYear;
     }
 }
